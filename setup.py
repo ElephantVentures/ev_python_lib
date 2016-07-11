742dc64dfcab936be593011fb51c66f4b589e1bf
Chris Bergstresser <chris@elephantventures.com>
2016-07-11 15:59:09 -0400

    Fix stray tab

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-	name = 'ev',
+  name = 'ev',
   version = '0.0.1',
   description = 'Elephant Ventures Libraries',
   url = 'https://github.com/ElephantVentures/ev_python_lib',
